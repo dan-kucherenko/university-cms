93c2fc92f51ed7a7b3113f1139a3229ded131ee0
Daniil Kucherenko <daniil.ukraine@gmail.com>
2023-10-17 23:48:36 +0300

    Initial commit of Spring Project

---
+++ src/main/java/ua/foxminded/kucherenko/task3/universitycms/UniversityCmsApplication.java
@@ -0,0 +1,13 @@
+package ua.foxminded.kucherenko.task3.universitycms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityCmsApplication.class, args);
+	}
+
+}
