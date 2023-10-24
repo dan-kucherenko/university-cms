a02616c764476a3f2b426d1e027e01e2f2319e3f
Daniil Kucherenko <daniil.ukraine@gmail.com>
2023-10-25 01:49:02 +0300

    Fix inheritance usage

--- src/main/java/ua/foxminded/kucherenko/task3/models/Student.java
+++ src/main/java/ua/foxminded/kucherenko/task3/models/Student.java
@@ -3,6 +3,8 @@ package ua.foxminded.kucherenko.task3.models;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Date;
+
 @Entity
 @Table(name = "students")
 @Getter
@@ -10,11 +12,23 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @RequiredArgsConstructor
-public class Student extends Person {
+public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter(AccessLevel.NONE)
     private int studentId;
+    @NonNull
+    private Integer id;
+    @NonNull
+    private String firstName;
+    @NonNull
+    private String lastName;
+    private Date dob;
+    private int age;
+    @NonNull
+    private String email;
+    @NonNull
+    private String phone;
     @NonNull
     private Integer yearOfStudy;
 }
--- src/main/java/ua/foxminded/kucherenko/task3/models/Teacher.java
+++ src/main/java/ua/foxminded/kucherenko/task3/models/Teacher.java
@@ -3,6 +3,8 @@ package ua.foxminded.kucherenko.task3.models;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Date;
+
 @Entity
 @Table(name = "teachers")
 @Getter
@@ -10,11 +12,23 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @RequiredArgsConstructor
-public class Teacher extends Person {
+public class Teacher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter(AccessLevel.NONE)
     private int teacherId;
+    @NonNull
+    private Integer id;
+    @NonNull
+    private String firstName;
+    @NonNull
+    private String lastName;
+    private Date dob;
+    private int age;
+    @NonNull
+    private String email;
+    @NonNull
+    private String phone;
     @NonNull
     @ManyToOne
     private Department department;
