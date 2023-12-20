07ce91d338e6b11a77c285b3535ae20e01afa756
Daniil Kucherenko <daniil.ukraine@gmail.com>
2023-12-20 14:22:30 +0200

    Remove getAllCourseIds

--- src/main/java/ua/foxminded/kucherenko/task3/repositories/CourseRepository.java
+++ src/main/java/ua/foxminded/kucherenko/task3/repositories/CourseRepository.java
@@ -1,14 +1,9 @@
 package ua.foxminded.kucherenko.task3.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ua.foxminded.kucherenko.task3.models.Course;
 
-import java.util.List;
-
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Integer> {
-    @Query("SELECT c.courseId FROM Course c")
-    List<Integer> getAllCourseIds();
 }
